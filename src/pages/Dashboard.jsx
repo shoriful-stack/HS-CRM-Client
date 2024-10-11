e3c77a98bd3db56fe63f53af9c6f523f312bad9a
Sa18-f <sisazid182713@gmail.com>
2024-10-12 02:30:49 +0600

    dashboards functionality and designed successfully

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -13,9 +13,17 @@ const Dashboard = () => {
     const [allProjects] = useAllProject();
     const [allEmployees] = useAllEmployee();
     return (
-        <div className='font-lexend'>
-            <h1 className='text-xl font-bold my-2'>Dashboard</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+        <div className='font-lexend px-2'>
+            <h1 className='text-2xl font-bold mt-1 mb-2'>Dashboard</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+                {/*Total Projects Card */}
+                <div className="bg-gradient-to-r from-[#000428] to-[#004e92] text-white shadow-lg rounded-lg px-5 py-4">
+                    <h2 className="text-lg font-semibold">Total Projects</h2>
+                    <div className="flex items-center justify-between mt-2">
+                        <RiProjectorFill className='text-3xl' />
+                        <span className="text-xl font-semibold">{allProjects.length}</span>
+                    </div>
+                </div>
                 {/* Total Tenders Card */}
                 <div className="bg-gradient-to-r from-[#11998e] to-[#38ef7d] shadow-lg rounded-lg px-5 py-4 text-white">
                     <h2 className="text-lg font-semibold">Total Contracts</h2>
@@ -23,18 +31,9 @@ const Dashboard = () => {
                         <FaMoneyCheckAlt className='text-[32px]' />
                         <span className="text-xl font-semibold">{allContracts.length}</span>
                     </div>
-                    {/* <p className="text-sm text-white">June 1st - July 1st</p> */}
-                </div>
-                {/*Total Users Card */}
-                <div className="bg-gradient-to-r from-indigo-900 to-[#4286f4] text-white shadow-lg rounded-lg px-5 py-4">
-                    <h2 className="text-lg font-semibold">Total Projects</h2>
-                    <div className="flex items-center justify-between mt-2">
-                        <RiProjectorFill className='text-3xl' />
-                        <span className="text-xl font-semibold">{allProjects.length}</span>
-                    </div>
                 </div>
                 {/* Employee Card */}
-                <div className="bg-gradient-to-r from-[#667db6] via-[#0082c8] to-[#667db6] text-white shadow-lg rounded-lg px-5 py-4">
+                <div className="bg-gradient-to-r from-[#43cea2] to-[#185a9d] text-white shadow-lg rounded-lg px-5 py-4">
                     <h2 className="text-lg font-semibold">Total Customers</h2>
                     <div className="flex items-center justify-between mt-2">
                         <FaUsers className='text-3xl' />
@@ -42,11 +41,44 @@ const Dashboard = () => {
                     </div>
                 </div>
                 {/* Inactive Users Card */}
-                <div className="bg-gradient-to-r from-[#FF0099] to-[#493240]  text-white shadow-lg rounded-lg px-5 py-4">
+                <div className="bg-gradient-to-r from-[#000428] to-[#004e92] text-white shadow-lg rounded-lg px-5 py-4">
                     <h2 className="text-lg font-semibold">Total Employees</h2>
                     <div className="flex items-center justify-between mt-2">
                         <GrUserWorker className='text-[26px] font-extrabold' />
                         <span className="text-xl font-semibold">{allEmployees.length}</span>
+                    </div>
+                </div>
+                {/*Total Users Card */}
+                <div className="bg-gradient-to-r from-[#000428] to-[#004e92] text-white shadow-lg rounded-lg px-5 py-4">
+                    <h2 className="text-lg font-semibold">Assigned Projects</h2>
+                    <div className="flex items-center justify-between mt-2">
+                        <RiProjectorFill className='text-3xl' />
+                        <span className="text-xl font-semibold">{allContracts.length}</span>
+                    </div>
+                </div>
+                {/* Total Tenders Card */}
+                <div className="bg-gradient-to-r from-[#11998e] to-[#38ef7d] shadow-lg rounded-lg px-5 py-4 text-white">
+                    <h2 className="text-lg font-semibold">Expired Contracts</h2>
+                    <div className="flex items-center justify-between mt-2">
+                        <FaMoneyCheckAlt className='text-[32px]' />
+                        <span className="text-xl font-semibold">{allContracts.filter(contract => (contract.contract_status) === "0" ).length}</span>
+                    </div>
+                    {/* <p className="text-sm text-white">June 1st - July 1st</p> */}
+                </div>
+                {/* Employee Card */}
+                <div className="bg-gradient-to-r from-[#43cea2] to-[#185a9d] text-white shadow-lg rounded-lg px-5 py-4">
+                    <h2 className="text-lg font-semibold">Active Customers</h2>
+                    <div className="flex items-center justify-between mt-2">
+                        <FaUsers className='text-3xl' />
+                        <span className="text-xl font-semibold">{allCustomers.filter(customer => (customer.status) === "1" ).length}</span>
+                    </div>
+                </div>
+                {/* Inactive Users Card */}
+                <div className="bg-gradient-to-r from-[#000428] to-[#004e92]  text-white shadow-lg rounded-lg px-5 py-4">
+                    <h2 className="text-lg font-semibold">Active Employees</h2>
+                    <div className="flex items-center justify-between mt-2">
+                        <GrUserWorker className='text-[26px] font-extrabold' />
+                        <span className="text-xl font-semibold">3</span>
                     </div>
                 </div>
             </div>
