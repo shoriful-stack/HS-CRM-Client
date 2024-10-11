802f87a4239f5bedc22e4a294c95ccd7a7548506
Sa18-f <sisazid182713@gmail.com>
2024-10-12 00:51:39 +0600

    useAllProject Hook created and dashboard page modified

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,50 +2,52 @@ import React from 'react';
 import { FaMoneyCheckAlt, FaUsers, FaShoppingCart } from 'react-icons/fa';
 import useAllContract from '../Hooks/useAllContract';
 import { GrUserWorker } from 'react-icons/gr';
+import useAllCustomer from '../Hooks/useAllCustomers';
+import { RiProjectorFill } from 'react-icons/ri';
+import useAllEmployee from '../Hooks/useAllEmployees';
+import useAllProject from '../Hooks/useAllProject';
 
 const Dashboard = () => {
     const [allContracts] = useAllContract();
+    const [allCustomers] = useAllCustomer();
+    const [allProjects] = useAllProject();
+    const [allEmployees] = useAllEmployee();
     return (
         <div className='font-lexend'>
-            <h1 className='text-2xl font-bold my-2 px-1'>Dashboard</h1>
+            <h1 className='text-xl font-bold my-2'>Dashboard</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
                 {/* Total Tenders Card */}
-                <div className="bg-gradient-to-r from-green-400 to-green-600 shadow-lg rounded-lg p-4 text-white">
-                    <h2 className="text-xl font-semibold">Total Contracts</h2>
-                    <div className="flex items-center justify-between mt-1">
-                        <FaMoneyCheckAlt className='text-4xl' />
-                        <span className="text-2xl font-bold">{allContracts.length}</span>
+                <div className="bg-gradient-to-r from-[#11998e] to-[#38ef7d] shadow-lg rounded-lg px-5 py-4 text-white">
+                    <h2 className="text-lg font-semibold">Total Contracts</h2>
+                    <div className="flex items-center justify-between mt-2">
+                        <FaMoneyCheckAlt className='text-[32px]' />
+                        <span className="text-xl font-semibold">{allContracts.length}</span>
                     </div>
                     {/* <p className="text-sm text-white">June 1st - July 1st</p> */}
                 </div>
-
                 {/*Total Users Card */}
-                <div className="bg-gradient-to-r from-cyan-500 to-blue-600 text-white shadow-lg rounded-lg p-4">
-                    <h2 className="text-xl font-semibold">Total Customers</h2>
-                    <div className="flex items-center justify-between mt-1">
-                        <FaUsers className='text-3xl' />
-                        <span className="text-2xl font-semibold">200</span>
+                <div className="bg-gradient-to-r from-indigo-900 to-[#4286f4] text-white shadow-lg rounded-lg px-5 py-4">
+                    <h2 className="text-lg font-semibold">Total Projects</h2>
+                    <div className="flex items-center justify-between mt-2">
+                        <RiProjectorFill className='text-3xl' />
+                        <span className="text-xl font-semibold">{allProjects.length}</span>
                     </div>
-                    {/* <p className="text-sm text-white">Active Users</p> */}
                 </div>
-
-                {/* Active Users Card */}
-                <div className="bg-gradient-to-r from-purple-500 to-pink-500 text-white shadow-lg rounded-lg p-4">
-                    <h2 className="text-xl font-semibold">Total Employees</h2>
-                    <div className="flex items-center justify-between mt-1">
-                        <GrUserWorker className='text-3xl' />
-                        <span className="text-2xl font-semibold">150</span>
+                {/* Employee Card */}
+                <div className="bg-gradient-to-r from-[#667db6] via-[#0082c8] to-[#667db6] text-white shadow-lg rounded-lg px-5 py-4">
+                    <h2 className="text-lg font-semibold">Total Customers</h2>
+                    <div className="flex items-center justify-between mt-2">
+                        <FaUsers className='text-3xl' />
+                        <span className="text-xl font-semibold">{allCustomers.length}</span>
                     </div>
-                    {/* <p className="text-sm text-white">Completed Orders</p> */}
                 </div>
                 {/* Inactive Users Card */}
-                <div className="bg-gradient-to-r from-purple-500 to-pink-500 text-white shadow-lg rounded-lg p-4">
-                    <h2 className="text-xl font-semibold">Inactive Users</h2>
-                    <div className="flex items-center justify-between mt-1">
-                        <FaShoppingCart className='text-4xl' />
-                        <span className="text-2xl font-semibold">150</span>
+                <div className="bg-gradient-to-r from-[#FF0099] to-[#493240]  text-white shadow-lg rounded-lg px-5 py-4">
+                    <h2 className="text-lg font-semibold">Total Employees</h2>
+                    <div className="flex items-center justify-between mt-2">
+                        <GrUserWorker className='text-[26px] font-extrabold' />
+                        <span className="text-xl font-semibold">{allEmployees.length}</span>
                     </div>
-                    {/* <p className="text-sm text-white">Completed Orders</p> */}
                 </div>
             </div>
         </div>
--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -1,8 +1,9 @@
 import { Button } from "flowbite-react";
 import { useState } from "react";
-import { FaFileContract, FaUsers } from "react-icons/fa";
+import { FaFileContract, FaMoneyCheckAlt, FaUsers } from "react-icons/fa";
 import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
 import { IoClose, IoMenu, IoSettings } from "react-icons/io5";
+import { RiProjectorFill } from "react-icons/ri";
 import { SiMastercomfig } from "react-icons/si";
 import { TbLogout2 } from "react-icons/tb";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
@@ -106,7 +107,7 @@ const Sidebar = () => {
                                 <NavLink to="/dashboard/home" end className={getNavLinkClass}>
                                     {/* Dashboard Icon */}
                                     <svg
-                                        className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"
+                                        className="ms-1 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"
                                         aria-hidden="true"
                                         xmlns="http://www.w3.org/2000/svg"
                                         fill="currentColor"
@@ -115,20 +116,20 @@ const Sidebar = () => {
                                         <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
                                         <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
                                     </svg>
-                                    {!isCollapsed && <span className="ms-3 text-white font-medium">Dashboard</span>}
+                                    {!isCollapsed && <span className="ms-[6px] text-white font-medium">Dashboard</span>}
                                 </NavLink>
                             </li>
                             <li>
                                 <NavLink to="/dashboard/master" className={getNavLinkClass}>
                                     {/* Master Icon */}
-                                    <SiMastercomfig className="text-base flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" />
+                                    <SiMastercomfig className="text-lg ms-1 flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" />
                                     {!isCollapsed && <span className="flex-1 ms-2 text-left text-white font-medium text-sm">Master Tree</span>}
                                 </NavLink>
                             </li>
                             <li>
                                 <NavLink to="/dashboard/projects" className={getNavLinkClass}>
                                     {/* Projects Icon */}
-                                    <svg
+                                    {/* <svg
                                         className="flex-shrink-0 w-4 h-4 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"
                                         aria-hidden="true"
                                         xmlns="http://www.w3.org/2000/svg"
@@ -136,14 +137,15 @@ const Sidebar = () => {
                                         viewBox="0 0 18 18"
                                     >
                                         <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286C10 17.169 10.831 18 11.857 18h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
-                                    </svg>
-                                    {!isCollapsed && <span className="ms-3 text-white">Projects</span>}
+                                    </svg> */}
+                                    <RiProjectorFill className="text-xl flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"/>
+                                    {!isCollapsed && <span className="ms-[10px] text-white">Projects</span>}
                                 </NavLink>
                             </li>
                             <li>
                                 <NavLink to="/dashboard/contracts" className={getNavLinkClass}>
                                     {/* Projects Icon */}
-                                    <FaFileContract className="text-[20px] flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" />
+                                    <FaMoneyCheckAlt className="text-lg ms-[2px] flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" />
                                     {!isCollapsed && <span className="flex-1 ms-2 text-left text-white font-medium text-sm">Contracts</span>}
                                 </NavLink>
                             </li>
@@ -155,7 +157,7 @@ const Sidebar = () => {
                                     <FaUsers className="text-xl flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" />
                                     {!isCollapsed && (
                                         <>
-                                            <span className="flex-1 ms-3 text-left text-white font-medium text-sm">CRM</span>
+                                            <span className="flex-1 ms-2 text-left text-white font-medium text-sm">CRM</span>
                                             {/* Arrow Icon */}
                                             {isDropdownOpen ? (
                                                 <IoIosArrowDown className="w-5 h-4 text-white" />
@@ -185,10 +187,10 @@ const Sidebar = () => {
                                     onClick={toggleSettingsDropdown}
                                     className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg dark:text-white dark:bg-gray-700 group dark:hover:bg-gray-700"
                                 >
-                                    <IoSettings className="text-xl flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" />
+                                    <IoSettings className="text-xl -ms-[2px] flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" />
                                     {!isCollapsed && (
                                         <>
-                                            <span className="flex-1 ms-3 text-left text-white font-medium text-sm">Settings</span>
+                                            <span className="flex-1 ms-2 text-left text-white font-medium text-sm">Settings</span>
                                             {/* Arrow Icon */}
                                             {isSettingsDropdownOpen ? (
                                                 <IoIosArrowDown className="w-5 h-4 text-white" />
